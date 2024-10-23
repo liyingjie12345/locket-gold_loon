8b68f5aa77059e84de408ced65f23cb71eafe32d
liyingjie12345 <2981081454b@gmail.com>
2024-10-23 13:33:59 +0700

    1

--- locket.js
+++ locket.js
@@ -37,7 +37,7 @@ if (typeof $response == "undefined") {
         let [e, s] = mapping[match];
         s ? (vuong2023.product_identifier = s, obj.body.subscriber.subscriptions[s] = ohoang7) : obj.body.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7, obj.body.subscriber.entitlements[e] = vuong2023
     } else obj.body.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7, obj.body.subscriber.entitlements.pro = vuong2023;
-    
+    obj.body = JSON.stringify(obj.body)
 }
 
 
