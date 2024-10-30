9ffe367ebbf002727b2e114f44575abbfe291695
liyingjie12345 <2981081454b@gmail.com>
2024-10-30 14:24:25 +0700

    Update notability.js

--- Notability/notability.js
+++ Notability/notability.js
@@ -9,16 +9,12 @@ var obj = {
         "tier": "premium",
         "isInBillingRetryPeriod": false,
         "refundedReason": null,
-        "expirationDate": "2025-11-28T13:03:53.000Z",
+        "expirationDate": "9999-11-28T13:03:53.000Z",
         "gracePeriodExpiresAt": null,
         "overDeviceLimit": false,
         "expirationIntent": null,
         "__typename": "AppStoreSubscription",
-        "user": {
-          "id": "40515d11-08cf-44ee-8b97-6a46cb79d1ae",
-          "email": "",
-          "__typename": "User"
-        },
+        "user": null,
         "status": "canceled",
         "originalPurchaseDate": "2023-11-28T13:03:56.000Z"
       },
